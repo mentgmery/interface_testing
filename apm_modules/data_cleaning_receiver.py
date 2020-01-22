d7c15f1d78ec9333d9137893745bd4d5bee68b90
benchenglian <benchenglian@foxmai.com>
2020-01-22 10:18:51 +0800

    增加触发器和接收器空值时验证

--- apm_modules/data_cleaning_receiver.py
+++ apm_modules/data_cleaning_receiver.py
@@ -11,94 +11,6 @@ from apm_modules.data_source_output import DataSourceOutput
 from apm_modules.template_mgt_output import TemplateMgtOutput
 from common.openbrowser import OpenURL
 
-data_source_list ={
-      "data-list": {
-        "name" : "数据文件列表",
-        "alias" : "data-list",
-        "getMethods" : {
-          "type" : "API",
-          "url" : "{{ APP_HOST }}/api/data_source",
-          "parameter" : {
-            "q_type": {
-              "type": "Number",
-              "default": 0
-            },
-            "page_size": 10,
-            "page": 1,
-            "case_id": "442791611"
-          }
-        },
-        "getContents": {
-          "category": '数据源/数据文件',
-          "fields": {
-          }
-        }
-      }
-    }
-
-data_source_details = {
-        "data-detail": {
-        "name": "数据文件详情",
-        "alias": "data-detail",
-        "getMethods": {
-          "type": "API",
-          "url": "{{ APP_HOST }}/lensData/lensData/getRawData",
-          "parameter": {
-            "id" : "3DA23E9F-F386-4A0D-87B1-25FFCD06A80D",
-            "case_id" : "442791611"
-          }
-        },
-        "getContents": {
-          "category": "数据源/数据文件详情",
-          "fields": {
-
-          }
-        }
-      }
-    }
-
-template_list = {
-      "data-list": {
-        "name" : "模板文件列表",
-        "alias" : "data-list",
-        "getMethods": {
-          "type" : 'API',
-          "url" : '{{ APP_HOST }}/api/template',
-          "parameter" : {
-            "page_size" : {
-              "type" : "Number",
-              "default" : 100
-            }
-          }
-        },
-        "getContents": {
-          "category": "数据源/数据文件",
-          "fields": {
-          }
-        }
-      }
-    }
-
-template_details =  {"data-detail": {
-        "name": "模板文件详情",
-        "alias": "data-detail",
-        "getMethods": {
-          "type": "API",
-          "url": "{{ APP_HOST }}/api/template",
-          "parameter": {
-            "temp_id": {
-              "type": "String",
-              "default": "971430007"
-            }
-          }
-        },
-        "getContents": {
-          "category": "数据源/数据文件详情",
-          "fields": {
-          }
-        }
-      }
-    }
 
 class DataCleaningReceiver(OpenURL):
 
@@ -118,34 +30,52 @@ class DataCleaningReceiver(OpenURL):
         return value
 
 
-    def get_dataclean_receiver_datalist_url(self):
+    def get_dataclean_receiver_datalist_url(self,data):
         """获取接收器—模板列表url
         :return: 返回请求的url 示例：http://192.168.1.244:8881/api/data_source?params=xxx
         """
         tmo_url = TemplateMgtOutput().templateMgt_list_validity().url
-        url = tmo_url + '&ENV_OUTPUT_TEMPLATE_LIST=' + str(template_list)
-        return url
+        if data is None:
+            url = tmo_url + '&ENV_OUTPUT_TEMPLATE_LIST={}'
+            return url
+        else:
+            url = tmo_url + '&ENV_OUTPUT_TEMPLATE_LIST=' + str(data)
+            return url
 
-    def get_dataclean_receiver_datadetails_url(self):
+
+    def get_dataclean_receiver_datadetails_url(self,data):
         """获取接收器—模板详情url
         :return: 返回请求的url 示例：http://192.168.1.244:8881/api/data_source?params=xxx
         """
         tmo_url = TemplateMgtOutput().templateMgt_details_validity().url
-        url = tmo_url + '&ENV_OUTPUT_DATA_DETAIL=' + str(template_details)
-        return url
+        if data is None:
+            url = tmo_url + '&ENV_OUTPUT_DATA_DETAIL={}'
+            return url
+        else:
+            url = tmo_url + '&ENV_OUTPUT_DATA_DETAIL=' + str(data)
+            return url
 
-    def get_template_list_url(self):
+    def get_template_list_url(self,data):
         """获取接收器—数据文件列表url
         :return: 返回请求的url 示例：http://192.168.1.244:8881/api/template?params=xxx
         """
         dso_url = DataSourceOutput().datasource_list_validity().url
-        url = dso_url + '&ENV_OUTPUT_DATA_LIST=' + str(data_source_list)
-        return url
+        if data is None:
+            url = dso_url + '&ENV_OUTPUT_DATA_LIST={}'
+            return url
+        else:
+            url = dso_url + '&ENV_OUTPUT_DATA_LIST=' + str(data)
+            return url
 
-    def get_template_details_url(self):
+
+    def get_template_details_url(self,data):
         """获取接收器—数据文件详情url
         :return: 返回请求的url 示例：http://192.168.1.244:8881/api/template?params=xxx
         """
         dso_url = DataSourceOutput().datasource_details_validity().url
-        url = dso_url + '&ENV_OUTPUT_DATA_DETAIL=' + str(data_source_list)
-        return url
+        if data is None:
+            url = dso_url + '&ENV_OUTPUT_DATA_DETAIL={}'
+            return url
+        else:
+            url = dso_url + '&ENV_OUTPUT_DATA_DETAIL=' + str(data)
+            return url
--- apm_modules/data_cleaning_trigger.py
+++ apm_modules/data_cleaning_trigger.py
@@ -11,18 +11,6 @@ from apm_modules.data_source_ops import DataSourceOps
 from common.openbrowser import OpenURL
 
 
-add_data = {
-        "add": {
-        "name": "新增数据",
-        "alias": "add",
-        "type" : 'window',
-        "metadata": {
-        },
-        "url": "http://admin.gongan.corp.elensdata.com/filemanagement?case_id=442791611",
-        "desc": '其他应用触发【新增数据】动作时，弹出【新增数据】window（小窗口），在小窗口中完成新增数据，触发父级页面刷新'
-      }
-    }
-
 class DataCleaningTrigger(OpenURL):
 
     def datacleaning_trigger_url(self,url,requiredDom):
@@ -40,11 +28,15 @@ class DataCleaningTrigger(OpenURL):
         return value
 
 
-    def get_datacleaning_trigger_add_data_url(self):
+    def get_datacleaning_trigger_add_data_url(self,data):
         """
         获取数据清洗触发器—url
         Returns: 返回get的url 示例：http://192.168.1.244:8881/dataetl?case_id=xxxx&sDid=xxxx&ENV_ACTION_ADD=xxxx
         """
         add_data_url=DataSourceOps(self.driver).get_datasource_add_data_url() # 获取数据源动作—新增数据url
-        url = add_data_url + '&ENV_ACTION_ADD' + str(add_data)
-        return url
+        if data is None:
+            url = add_data_url + '&ENV_ACTION_ADD={}'
+            return url
+        else:
+            url = add_data_url + '&ENV_ACTION_ADD=' + str(data)
+            return url
